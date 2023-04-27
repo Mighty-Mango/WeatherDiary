a9b302ba20614c7ac3586838a813d66ec940ad87
mad-pow <powers.madison@gmail.com>
2023-04-27 19:02:35 -0400

    Updated API Key + Getting city,state
    currently city,state is just a string need to actually pass it between activities.

--- app/src/main/java/com/example/weatherdiary/MapsActivity.kt
+++ app/src/main/java/com/example/weatherdiary/MapsActivity.kt
@@ -2,11 +2,15 @@ package com.example.weatherdiary
 
 import android.Manifest
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.location.Location
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import androidx.annotation.RequiresApi
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 
@@ -37,7 +41,9 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
     private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
     private var lastKnownLocation: Location? = null
     private val defaultLocation = LatLng(38.9869, -76.9426) //UMD
-
+    private lateinit var locButton : Button
+    private lateinit var listButton : Button
+    private var city_state : String = "Not Set Yet"
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,18 +59,32 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
-    }
+        locButton = findViewById(R.id.gotoview2)
+        listButton = findViewById(R.id.gotoview3)
 
+
+        locButton.setOnClickListener{
+            Log.w("BUTTON", "Go to second view")
+            Log.w("BUTTON", "City, State is " + city_state)
+        }
+
+        listButton.setOnClickListener{
+            Log.w("BUTTON", "Go to third view")
+        }
+
+    }
 
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
      * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
+     * we just add a marker at UMD.
      * If Google Play services is not installed on the device, the user will be prompted to install
      * it inside the SupportMapFragment. This method will only be triggered once the user has
      * installed Google Play services and returned to the app.
      */
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
         mMap.uiSettings.isZoomControlsEnabled = true //allow zoom buttons
@@ -73,15 +93,28 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
         getDeviceLocation()
 
 
+        var geocoder : Geocoder = Geocoder( this )
+        var handler : GeocodingHandler = GeocodingHandler()
+
+        if(::locMarker.isInitialized){
+            Log.w("TESTING", "MARKER HAS A VALUE")
+        }else{
+            Log.w("TESTING", "MARKER IS EMPTY")
+        }
+
         mMap.setOnMapClickListener {
             Log.w("TESTING", "LatLong is: " + it)
             mMap.clear()
             locMarker = mMap.addMarker(MarkerOptions().position(it))!! //lets see if this causes issues
             mMap.moveCamera(CameraUpdateFactory.newLatLng(it)) //center camera
+
+            geocoder.getFromLocation(locMarker.position.latitude,
+                locMarker.position.longitude, 5, handler)
         }
 
-        //have two buttons for navi between second and third views
+
     }
+
 
     //below functions are taken from Google Maps API example code
     private fun getLocationPermission() {
@@ -105,7 +138,6 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
         locationPermissionGranted = false
         when (requestCode) {
             1 -> {
-
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.isNotEmpty() &&
                     grantResults[0] == PackageManager.PERMISSION_GRANTED) {
@@ -125,6 +157,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
             if (locationPermissionGranted) {
                 mMap?.isMyLocationEnabled = true
                 mMap?.uiSettings?.isMyLocationButtonEnabled = true
+
                 Log.w("TESTING", "Permission Granted")
             } else {
                 mMap?.isMyLocationEnabled = false
@@ -137,6 +170,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
         }
     }
 
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
     private fun getDeviceLocation() {
         /*
          * Get the best and most recent location of the device, which may be null in rare
@@ -153,6 +187,20 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
                             mMap?.moveCamera(CameraUpdateFactory.newLatLngZoom(
                                 LatLng(lastKnownLocation!!.latitude,
                                     lastKnownLocation!!.longitude), DEFAULT_ZOOM.toFloat()))
+                            //below places default marker at lastknownlocation/currentlocation
+                            mMap.clear()
+                            locMarker = mMap.addMarker(
+                                MarkerOptions().position(
+                                    LatLng(lastKnownLocation!!.latitude,lastKnownLocation!!.longitude)))!!
+                            Log.w("TESTING", "MARKER HAS BEEN SET TO LAST LOC")
+
+                            //adding geocoder code here to ensure phone location can be chosen w/o user clicking
+
+                            var geocoder : Geocoder = Geocoder( this )
+                            var handler : GeocodingHandler = GeocodingHandler()
+                            geocoder.getFromLocation(locMarker.position.latitude,
+                                locMarker.position.longitude, 5, handler)
+
                         }
                     } else {
                         mMap?.moveCamera(CameraUpdateFactory
@@ -165,7 +213,27 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback{
             Log.e("Exception: %s", e.message, e)
         }
     }
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    inner class GeocodingHandler : Geocoder.GeocodeListener {
+        override fun onGeocode(addresses: MutableList<Address>) {
+            if( addresses != null ) {
+                var latitude: Double = addresses[0].latitude
+                var longitude: Double = addresses[0].longitude
+                var entireAdd : String = addresses[0].getAddressLine(0) //entire add
+                var cityAdd : String = addresses[0].locality //city
+                var stateAdd : String = addresses[0].adminArea //state
+                city_state = "$cityAdd, $stateAdd"
+
+                Log.w("TESTING", "City and state: " + city_state)
+                Log.w("MainActivity", "(lat,long) = ($latitude, $longitude)")
+            } else
+                Log.w( "MainActivity", "Sorry, no results" )
+        }
+
+    }
+
     companion object {
-        private const val DEFAULT_ZOOM = 15
+        private const val DEFAULT_ZOOM = 16
     }
 }
