3e3faffd515984a38a9c517788f7b08a307bcc0e
jmaloy315 <jmaloy@terpmail.umd.edu>
2023-05-05 13:04:32 -0400

    Added Date Picker

--- app/src/main/java/com/example/weatherdiary/SecondActivity.kt
+++ app/src/main/java/com/example/weatherdiary/SecondActivity.kt
@@ -1,7 +1,13 @@
 package com.example.weatherdiary
 
+import android.app.AlertDialog
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -9,18 +15,5 @@ class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
-
-        var recyclerView : RecyclerView = findViewById(R.id.recyclerview)
-
-        var items = arrayListOf<Item>()
-        items.add(Item("Sunny", R.drawable.sunny))
-        items.add(Item("Sunny With Rain", R.drawable.sunnyrainy))
-        items.add(Item("Thunderstorms", R.drawable.thunderstorm))
-        items.add(Item("Snow", R.drawable.cloudysnowy))
-        items.add(Item("Partly Cloudy", R.drawable.partlycloudy))
-
-
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = MyAdapter(applicationContext, items)
     }
 }
